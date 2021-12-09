dd907f2c9ffc3b7f51de98957e58b977156a6b4a
Arif5H <arifhh@code.edu.az>
2021-12-09 21:03:12 +0400

    Добавьте файлы проекта.

---
+++ Task1/Class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Class
+    {
+        public double SumNumbers(double a, double b)
+        {
+            double sum = a + b;
+
+            return sum;
+
+        }public double DeductNumbers(double a, double b)
+        {
+            double deduct = a - b;
+
+            return deduct;
+
+        }public double MultiplyNumbers(double a, double b)
+        {
+            double multiply = a * b;
+
+            return multiply;
+
+        }
+        
+        public double DevideNumbers(double a,double b)
+        {
+            double devide =a/b;
+            
+
+           
+            return devide;
+
+        }
+
+
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            Class class1 = new Class();
+           step1:
+            Console.Write("Please Select an action :[+], [-], [*], [/] ");
+            string action = Console.ReadLine();
+            if (action == "+" || action == "-" || action == "*" || action == "/")
+            {
+                Console.Write("Please enter 1st variable ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+            step2:
+                Console.Write("Please enter 2nd variable ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+                double result;
+                switch (action)
+                {
+                    case "+":
+                        result = class1.SumNumbers(num1, num2);
+                        break;
+                    case "-":
+                        result = class1.DeductNumbers(num1, num2);
+
+                        break;
+                    case "*":
+                        result = class1.MultiplyNumbers(num1, num2);
+
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                        {
+                            result = class1.DevideNumbers(num1, num2);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Can't Devide To 0");
+                            Console.WriteLine("Please, push any button and TRY AGAIN");
+                            Console.ReadKey();
+                            goto step2;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Selected acction is none of recommended: [+], [-], [*], [/]");
+                        Console.WriteLine("Please, push any button and TRY AGAIN");
+                        Console.ReadKey();
+                        goto step1;
+
+                }
+                        Console.WriteLine($"The Result of Calculation is: {result}");
+
+            }
+            else
+            {
+                        Console.WriteLine("Selected acction is none of recommended: [+], [-], [*], [/]");
+                        Console.WriteLine("Please, push any button and TRY AGAIN");
+                        Console.ReadKey();
+                        goto step1;
+
+            }
+            Console.ReadKey();
+
+        }
+           
+
+    }
+ }
+
